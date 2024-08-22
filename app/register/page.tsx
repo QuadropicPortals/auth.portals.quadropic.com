d9bfc8eef7bb273078024d62dc2b2bc6da2c4682
Mohamed Kamran <mohamedkamran2211@icloud.com>
2024-08-22 13:56:59 +0530

    refactor: Update registration confirmation page to use OTP for authentication

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -45,7 +45,7 @@ export default function LoginPage() {
         setError("There's something wrong. Please try again later.");
         return;
       }
-      router.replace("register/otpconfirm");
+      router.replace("/otpconfirm");
       // Handle the response
     } catch (error) {
       setError("There's something wrong. Please try again later.");
